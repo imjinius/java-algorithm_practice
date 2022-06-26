6c92a33add68f68d1f4076d1306c91bfcc7511ec
Hyejin Ha <gpwlsdlsms50@naver.com>
2022-06-26 13:53:44 +0900

    first commit

---
+++ exception.java
@@ -0,0 +1,54 @@
+class PasswordException extends Exception{
+    PasswordException(String msg){
+        super(msg);
+    }
+}
+
+public class exception {
+    String password;
+
+    public void setUserPw(String pw) throws PasswordException{
+        if(pw == null){
+            throw new PasswordException("비밀번호는 null일수 없습니다.");
+        }
+        else if(pw.length()<5){
+            throw new PasswordException("비밀번호는 5자 이상이어야 합니다.");
+        }
+        else if(pw.matches("[a-zA-Z]+")) {
+            throw new PasswordException("비밀번호는 숫자,문자를 조합하여야 합니다.");  
+        }
+    }
+
+    public static void main(String[] args){
+        exception ex = new exception();
+        String pw = null;
+
+        try {
+            ex.setUserPw(pw);
+        } catch (PasswordException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            e.getMessage();
+        }
+
+        pw = "123";
+        try {
+            ex.setUserPw(pw);
+        } catch (PasswordException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            e.getMessage();
+        }
+        
+        pw = "abcdef";
+        try {
+            ex.setUserPw(pw);
+        } catch (PasswordException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            e.getMessage();
+        }
+    }
+    
+
+}
